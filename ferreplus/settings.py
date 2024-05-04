0d0fdf5d2150b7feb879875145e451c0fc6c880d
Marina Comotti <m.comotti@hotmail.com>
2024-05-04 15:35:38 -0300

    Add sign up

--- ferreplus/settings.py
+++ ferreplus/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -129,4 +131,4 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 
 
 LOGIN_REDIRECT_URL = "home"
-LOGOUT_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"   #ESTO NO ANDA !! Falta ver que pasa y arreglarlo
--- ferreplus/urls.py
+++ ferreplus/urls.py
@@ -22,5 +22,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     # User management
     path("accounts/", include("django.contrib.auth.urls")),
+    # Local apps
+    path("accounts/", include("accounts.urls")),
     path("", include("pages.urls")),
 ]
