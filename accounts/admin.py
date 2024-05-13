152f53c94a60891b24d117533864ccd8c0a47ac1
Javier Ballesteros <javoballesteros2016@gmail.com>
2024-05-13 15:45:54 -0300

    Acomodando

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
+from django.contrib import admin
+from .models import CustomUser
+
+# Register your models here.
+admin.site.register(CustomUser)
+
 #from django.contrib import admin
 #from django.contrib.auth import get_user_model
 #from django.contrib.auth.admin import UserAdmin
--- accounts/create_groups.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import Group
-
-def create_groups():
-    Group.objects.get_or_create(name='admi')
-    Group.objects.get_or_create(name='client')
-    Group.objects.get_or_create(name='employee')
-
-if __name__ == '__main__':
-    create_groups()
