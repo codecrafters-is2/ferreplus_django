6f695b4e8ae042c01cb6ccb9a1c18a4afa53fa95
Javier Ballesteros <javoballesteros2016@gmail.com>
2024-05-15 11:43:41 -0300

    subo para bajar la update

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,6 @@
-from django.urls import path
-from accounts.views import ClientPasswordChangeView,CustomPasswordChangeView,ChangePasswordView
+#from django.urls import path
+#from accounts.views import MyPasswordChangeView
 
-urlpatterns = [
-    #path('password/change/', CustomPasswordChangeView.as_view(), name='password_change'),
-    path('change-password/', ChangePasswordView.as_view(), name='password_change'),
-]
+#urlpatterns = [
+#    path('password/change/', MyPasswordChangeView.as_view(), name='password_change'),
+#]
--- accounts/views.py
+++ accounts/views.py
@@ -1,38 +1,13 @@
+
+#from django.contrib import messages
 #from django.contrib.auth.views import PasswordChangeView
-#from django.shortcuts import redirect
 #from django.urls import reverse_lazy
 
-
-
-#class ClientPasswordChangeView(ClientRequiredMixin, PasswordChangeView):
-#    success_url = reverse_lazy('password_change_done')
-
-#class CustomPasswordChangeView(ClientRequiredMixin, PasswordChangeView):
-#    success_url = reverse_lazy('password_change_done')
-    
-from django.contrib import messages
-from django.contrib.auth import update_session_auth_hash
-from django.contrib.auth.forms import PasswordChangeForm
-from django.shortcuts import render, redirect
-from django.views import View
-from accounts.mixins import ClientRequiredMixin
-
-class ChangePasswordView(ClientRequiredMixin,View):
-    template_name = 'password_change.html'
-
-    def get(self, request):
-        form = PasswordChangeForm(request.user)
-        return render(request, self.template_name, {'form': form})
-
-    def post(self, request):
-        form = PasswordChangeForm(request.user, request.POST)
-        if form.is_valid():
-            user = form.save()
-            update_session_auth_hash(request, user)  # Importante para mantener al usuario conectado
-            messages.success(request, '¡Tu contraseña se actualizó correctamente!')
-            password_success_message = '¡Tu contraseña se actualizó correctamente!'
-            return redirect('home')
-        
-        # Si el formulario no es válido, conservamos los mensajes por defecto de Django
-        return render(request, self.template_name, {'form': form, 'password_success_message': None})
-
+#class MyPasswordChangeView(PasswordChangeView):
+#    template_name = "password_change.html"
+#    success_url = reverse_lazy('password_change_done')  # Redirecciona aquí después del cambio de contraseña
+#    #success_url = '/accounts/password/change/successful/'
+#    def form_valid(self, form):
+#        response = super().form_valid(form)
+#        messages.success(self.request, '¡Contraseña cambiada con éxito!')
+#        return response
