c087df1218b20246d7ea17034ceff83186c164df
Javier Ballesteros <javoballesteros2016@gmail.com>
2024-05-14 21:38:23 -0300

    Extension de navbar e intento de informar exito al cambio de ocntraseña

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from accounts.views import ClientPasswordChangeView,CustomPasswordChangeView
+from accounts.views import ClientPasswordChangeView,CustomPasswordChangeView,ChangePasswordView
 
 urlpatterns = [
-    path('password/change/', CustomPasswordChangeView.as_view(), name='password_change'),
+    #path('password/change/', CustomPasswordChangeView.as_view(), name='password_change'),
+    path('change-password/', ChangePasswordView.as_view(), name='password_change'),
 ]
