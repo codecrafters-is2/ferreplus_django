57b103efd0c1b331d16ba6f3504add179e3615cd
Numa-diazdiva <manuel.diazd@hotmail.com>
2024-05-15 22:30:42 -0300

    Ultimos cambios en templates

--- accounts/management/commands/create_user_groups.py
+++ accounts/management/commands/create_user_groups.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
-from django.contrib.auth.models import Group
-
+from django.contrib.auth.models import User, Group
+from django.contrib.auth import get_user_model
+from accounts.models import CustomUser
 
 class Command(BaseCommand):
     help = "Crea los grupos que se requieren para asignar a los usuarios"
@@ -13,6 +14,13 @@ class Command(BaseCommand):
             Group.objects.get_or_create(name='admi')
             Group.objects.get_or_create(name='client')
             Group.objects.get_or_create(name='employee')
+
+            User = get_user_model()
+            User.objects.create_user('admi', 'ferreplusgestion@gmail.com', 'admi')
+            usuario = CustomUser.objects.get(username='admi')
+            grupo = Group.objects.get(name='admi')
+            usuario.groups.add(grupo)
+
             self.stdout.write(
                 self.style.SUCCESS("Grupos creados exitosamente")
             )
