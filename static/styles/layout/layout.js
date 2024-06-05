389ad3f9c77ac0514080aaf31d4c316c48f2083a
Numa-diazdiva <manuel.diazd@hotmail.com>
2024-06-05 00:59:41 -0300

    Un poco más de home. Fix navbar en modo celular. Comando para cargar carousel

--- static/styles/layout/layout.js
+++ static/styles/layout/layout.js
@@ -3,21 +3,22 @@ document.addEventListener('DOMContentLoaded', initialize);
 
 function initialize() {
   const button = document.querySelector(".hamburguer-button");
-  const cellphoneMenu = document.querySelector(".navbar-nav");
+  const cellphoneMenu = document.querySelector(".navbar-navigation");
 
   button.addEventListener('click', () => {  
-    cellphoneMenu.classList.contains("visible") ? cellphoneMenu.classList.remove("visible") : cellphoneMenu.classList.add("visible");
+    cellphoneMenu.classList.contains("custom-visible") ? cellphoneMenu.classList.remove("custom-visible") : cellphoneMenu.classList.add("custom-visible");
   })
 
   window.addEventListener("resize", () => {
-    window.innerWidth > 768 && cellphoneMenu.classList.remove("visible");
+    window.innerWidth > 768 && cellphoneMenu.classList.remove("custom-visible");
   })
 
   const userButton = document.getElementById("user-button");
   const userMenu = document.querySelector(".user-menu");
 
-  userButton.addEventListener('click', () => {
-    userMenu.classList.contains("shown") ? userMenu.classList.remove("shown") : userMenu.classList.add("shown");
-  })
-
+  if (userButton) {
+    userButton.addEventListener('click', () => {
+      userMenu.classList.contains("shown") ? userMenu.classList.remove("shown") : userMenu.classList.add("shown");
+    });
+  }
 }
